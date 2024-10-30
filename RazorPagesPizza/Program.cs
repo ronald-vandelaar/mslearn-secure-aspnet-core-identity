5ff9446e4f8d595dfacc2a21d415d65724c64a84
ronald-vandelaar <82531379+ronald-vandelaar@users.noreply.github.com>
2024-10-30 18:25:39 +0000

    Email Changes

--- RazorPagesPizza/Program.cs
+++ RazorPagesPizza/Program.cs
@@ -1,7 +1,19 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesPizza.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using RazorPagesPizza.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("RazorPagesPizzaAuthConnection") ?? throw new InvalidOperationException("Connection string 'RazorPagesPizzaAuthConnection' not found.");
+
+builder.Services.AddDbContext<RazorPagesPizzaAuth>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<RazorPagesPizzaUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<RazorPagesPizzaAuth>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddTransient<IEmailSender, EmailSender();
 
 var app = builder.Build();
 
